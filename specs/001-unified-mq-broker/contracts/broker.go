e9f80545ba36b419b37b3c637a2b98ac12e025ff
Easton <uaston@a.com>
2026-01-20 20:44:28 +0800

    update

--- specs/001-unified-mq-broker/contracts/broker.go
+++ specs/001-unified-mq-broker/contracts/broker.go
@@ -1,42 +1,27 @@
 package contracts
-// Package broker defines the unified messaging contracts.
-package broker
 
+import "context"
 
+// Broker is the core interface for MQ interactions.
+type Broker interface {
+	// Init initializes the broker with options.
+	Init(opts ...any) error
+	// Options returns the current broker options.
+	Options() any
+	// Connect establishes the physical connection to the MQ server.
+	Connect() error
+	// Disconnect closes the connection and cleans up resources.
+	Disconnect() error
+	// Publish sends a message to a topic.
+	Publish(ctx context.Context, topic string, msg any, opts ...any) error
+	// Subscribe registers a handler for a topic.
+	Subscribe(topic string, handler any, opts ...any) (Subscriber, error)
+	// String returns the name of the broker implementation.
+	String() string
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}	Unsubscribe() error	Topic() string	Options() SubscribeOptionstype Subscriber interface {// Subscriber represents a subscription handle.}	Error() error	Ack() error	Message() *Message	Topic() stringtype Event interface {// Event is the interface for received messages.type Handler func(context.Context, Event) error// Handler is the function called when a message is received.}	String() string	// String returns the name of the broker implementation.	Subscribe(topic string, handler Handler, opts ...SubscribeOption) (Subscriber, error)	// Subscribe registers a handler for a topic.	Publish(ctx context.Context, topic string, msg *Message, opts ...PublishOption) error	// Publish sends a message to a topic.	Disconnect() error	// Disconnect closes the connection and cleans up resources.	Connect() error	// Connect establishes the physical connection to the MQ server.	Options() Options	// Options returns the current broker options.	Init(opts ...Option) error	// Init initializes the broker with options.type Broker interface {// Broker is the core interface for MQ interactions.import "context"
+type Subscriber interface {
+	Options() any
+	Topic() string
+	Unsubscribe() error
+}
